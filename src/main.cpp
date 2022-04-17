dde927f06f5e7c93ebaf1c305e8c3e8d77aa8ce9
Manny Peterson <12462046+MannyPeterson@users.noreply.github.com>
2022-04-17 10:46:57 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -7,31 +7,23 @@
 #include "/home/manny/Documents/misc/secrets.h"
 
 #define DOOR_SW_PIN 17
+#define SERVER "ec2-44-234-32-170.us-west-2.compute.amazonaws.com"
+#define PRODUCT_ID "INSIGNIA%20NS%2DCF26WH9"
+#define CUSTOMER_ID "8923742934234"
+#define SERIAL_NO "21G07W00331"
 
-void read_temp(void);
-void event_send(void);
+void read_temp(double *tmp_);
+void event_send(double *tmp_);
 
 const char *ssid = SSID;
 const char *pass = PASS;
 
-String host = "ec2-44-234-32-170.us-west-2.compute.amazonaws.com";
-
-String product_id = "INSIGNIA%20NS%2DCF26WH9";
-String customer_id = "8923742934234";
-String serial_no = "21G07W00331";
-double temperature = 0.0;
-
-HTTPClient http;
 TMP117 tmp117;
 
 void setup()
 {
 
   Serial.begin(115200);
-
-  Wire.begin(SDA, SCL);
-  Wire.setClock(400000);
-  tmp117.begin();
 
   pinMode(DOOR_SW_PIN, INPUT_PULLUP);
 
@@ -50,31 +42,39 @@ void loop()
 {
 }
 
-void read_temp(void)
+void read_temp(double *tmp_)
 {
+
+  Wire.begin(SDA, SCL);
+  Wire.setClock(400000);
+  tmp117.begin();
 
   while (false == tmp117.dataReady())
   {
     delay(500);
   }
 
-  temperature = tmp117.readTempF();
+  *tmp_ = tmp117.readTempF();
 
   return;
 }
 
-void event_send(void)
+void event_send(double *tmp_)
 {
+  HTTPClient client;
+  String server = String(SERVER);
+  String product_id = String(PRODUCT_ID);
+  String customer_id = String(CUSTOMER_ID);
+  String serial_no = String(SERIAL_NO);
+  String tmp = String(*tmp_, 3);
 
-  String request = "http://" + host + "/event%5Freceive.php?cid=" + customer_id + "&pid=" + product_id + "&sn=" + serial_no + "&tmp=" + String(temperature, 3);
+  String request = String("http://" + server + "/event%5Freceive.php?cid=" + customer_id + "&pid=" + product_id + "&sn=" + serial_no + "&tmp=" + tmp);
 
-  http.begin(request.c_str());
+  client.begin(request.c_str());
 
-  http.GET();
+  client.GET();
 
-  Serial.println(http.getString());
-
-  http.end();
+  client.end();
 
   return;
 }
